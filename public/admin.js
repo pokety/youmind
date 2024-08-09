3bef95eb3e667757bb238d107e20c1fa9795352b
claudio amilton <pokety@gmail.com>
2024-08-09 11:33:15 +0000

    preview perguntas

--- public/admin.js
+++ public/admin.js
@@ -7,7 +7,7 @@ fetch('/questions').then((resp)=>resp.json()).then((result)=>{
     
     let lista=document.querySelector("#lista")
     result.forEach(el => {
-        lista.innerHTML+=`<li onclick="selectQuestion(${el.id})" class="flex items-center space-x-3 rtl:space-x-reverse">
+        lista.innerHTML+=`<li  onclick="selectQuestion(this)" class="flex items-center space-x-3 rtl:space-x-reverse">
         <svg class="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5"/>
         </svg>
@@ -19,9 +19,14 @@ fetch('/questions').then((resp)=>resp.json()).then((result)=>{
 })
 
 function selectQuestion(opts){
+
     let pergunta=document.querySelector("#pergunta")
-    pergunta.value=opts
-}
+    let prevPergunta=document.querySelector("#prevPergunta")
+    pergunta.value=opts.children[1].innerText
+    opts.children[2].setAttribute('class','line-through')
+    prevPergunta.innerText=opts.children[2].innerText
+    prevPergunta.style.display='block'
+    }
 
 enviar.addEventListener("click",()=>{
     let pergunta=document.querySelector("#pergunta")
